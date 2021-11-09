64e117fb687039050364b0eed2658dba75a07615
AJ <aj@smartnvm.com>
2021-11-09 13:02:33 -0500

    - add code comments

--- player.rb
+++ player.rb
@@ -1,4 +1,8 @@
 class Player
+  #Objects method are public, whereas Data is private
+  #to access data we need getter and setter, i.e.
+  #attr_reader and attr_writer
+  #attr_accessor short-hand for attr_reader / attr_writer
   attr_accessor :name, :lives
   def initialize(name)
     @name = name
--- question.rb
+++ question.rb
@@ -1,4 +1,5 @@
 class Question
+  #short hand for attr_writer and attr_reader
   attr_accessor :value1, :value2, :answer
   def initialize 
     @value1 = rand(1..50)
