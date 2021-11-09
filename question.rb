58cde01842992bbd2b3cea68366de70c999a4b43
AJ <aj@smartnvm.com>
2021-11-09 13:12:10 -0500

    - sync repo with remote

--- question.rb
+++
@@ -1,9 +0,0 @@
-class Question
-  #short hand for attr_writer and attr_reader
-  attr_accessor :value1, :value2, :answer
-  def initialize 
-    @value1 = rand(1..50)
-    @value2 = rand(1..50)
-    @answer = @value1 + @value2
-  end
-end
